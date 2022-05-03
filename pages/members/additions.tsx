c42162a8121fd4afeb0adc834c4278bb35e3b913
Codeliner <codeliner96@gmail.com>
2022-05-04 00:02:52 +0900

    [v1.7.3] Solved member addition page more button loading.

--- pages/members/additions.tsx
+++ pages/members/additions.tsx
@@ -173,12 +173,36 @@ const Additions: NextPage = () => {
                   onFollowClick={() => onFollowingClick(member.id)}
                 />
               ))}
-              <button
-                className="text-purple-500 tracking-widest hover:scale-110 hover:text-purple-600 transition-transform p-2"
-                onClick={onMoreClick}
-              >
-                more
-              </button>
+              <div className="pt-2 flex justify-center items-center">
+                {isMoreLoading ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6 animate-spin"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                ) : (
+                  <button
+                    className="text-purple-500 tracking-widest hover:scale-110 hover:text-purple-600 transition-transform"
+                    onClick={onMoreClick}
+                  >
+                    more
+                  </button>
+                )}
+              </div>
             </div>
           ) : (
             <span className="flex justify-center m-20 text-gray-400 tracking-widest text-sm cursor-default whitespace-nowrap hover:scale-105 transition">
